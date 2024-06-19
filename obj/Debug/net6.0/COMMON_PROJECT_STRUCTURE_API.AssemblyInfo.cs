5d6eac51db838b970ddfa8510b2dcd947ab9d8ef
Akash Kumar <73817248+Akash581999@users.noreply.github.com>
2024-06-19 18:42:17 +0530

    Fixed resetpassword API

--- obj/Debug/net6.0/COMMON_PROJECT_STRUCTURE_API.AssemblyInfo.cs
+++ obj/Debug/net6.0/COMMON_PROJECT_STRUCTURE_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMMON_PROJECT_STRUCTURE_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1086f75596b83116c0cce95462ab6efa0b9c8efa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23200ab75cd909d5719af5309322ad7aeabb3fb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMMON_PROJECT_STRUCTURE_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMMON_PROJECT_STRUCTURE_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
